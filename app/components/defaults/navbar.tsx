2bb490c23cf9eef9fe293825e57055b44586fe46
henrikhaus <henrik.utistog.hausberg@outlook.com>
2023-11-06 17:17:17 +0100

    Header start

--- app/components/defaults/navbar.tsx
+++ app/components/defaults/navbar.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
 export default function Navbar() {
-    return (
-        <nav>
-            <h1>Header</h1>
-        </nav>
-    )
+  return (
+    <nav className='bg-slate-950'>
+      <div className='bg-dark flex justify-between p-3 align-middle content-center "ml-60 mr-60" shadow-xl'>
+        <button className='bg-dark w-12 h-12 border-r-secondary float-left'>
+          <span className='material-icons-round text-primary text-6xl'>
+            menu
+          </span>
+        </button>
+        <Image
+          src='/icons/logo_jub.svg'
+          alt=''
+          className='mt-2'
+          width={240}
+          height={48}
+        />
+        <div className='w-16 h-16' />
+      </div>
+      <div id='sideBar' className='h-screen w-32 bg-dark hidden'></div>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <h2>Dashboard</h2>
+    <main className='"bg-slate-950 justify-center h-screen"'>
+      <div className='"bg-slate-600 ml-60 mr-60 h-screen"'>
+        <h2>Dashboard</h2>
+      </div>
     </main>
-  )
+  );
 }
