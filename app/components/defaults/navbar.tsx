fd1cba113452d0a4980b5131277c397897b82c37
Anders <anders.morille@gmail.com>
2023-11-13 19:49:34 +0100

    Fixed navbar

--- app/components/defaults/navbar.tsx
+++ app/components/defaults/navbar.tsx
@@ -5,10 +5,7 @@ import ThemeToggler from '../ThemeToggler';
 export default function Navbar() {
   return (
     <nav className="bg-slate-950">
-    <nav className="bg-slate-950">
       <div className='bg-dark flex justify-between p-3 align-middle content-center "ml-60 mr-60" shadow-xl'>
-        <button className="bg-dark w-12 h-12 border-r-secondary float-left">
-          <span className="material-icons-round text-primary text-6xl">
         <button className="bg-dark w-12 h-12 border-r-secondary float-left">
           <span className="material-icons-round text-primary text-6xl">
             menu
@@ -16,15 +13,12 @@ export default function Navbar() {
         </button>
         <Logo
           className="fill-green-500 dark:fill-green-200"
-          className="fill-green-500 dark:fill-green-200"
           width={200}
-          height={'auto'}
           height={'auto'}
         />
         <div className="w-16 h-16" />
         <ThemeToggler />
       </div>
-      <div id="sideBar" className="h-screen w-32 bg-dark hidden"></div>
       <div id="sideBar" className="h-screen w-32 bg-dark hidden"></div>
     </nav>
   );
