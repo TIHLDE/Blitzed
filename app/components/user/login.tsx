e258bface10ab453fad4e3e74142fecd0bcdda40
Anders <69890033+lille-morille@users.noreply.github.com>
2024-01-08 22:30:26 +0100

    Current Shadcn-ui migration (#16)
    - Implemented and migrated to Shadcn-UI library
    
    ---------
    
    Co-authored-by: henrikhaus <henrik.utistog.hausberg@outlook.com>

--- app/components/user/login.tsx
+++ app/components/user/login.tsx
@@ -1,19 +1,20 @@
 'use client';
 
 import { useUser } from '@/app/hooks/useUser';
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from "react";
-import Input from '@/app/components/defaults/input';
-import {getClientCookie } from '@/app/utils/stores/cookieStore';
-import {useAuth} from "@/app/user/auth/context/AuthContext";
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { getClientCookie } from '@/app/utils/stores/cookieStore';
+import { useAuth } from '@/app/user/auth/context/AuthContext';
+import Input from '@/components/layout/input';
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const { login } = useUser();
-  const router = useRouter()
+  const router = useRouter();
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const { isLoggedIn } = useAuth();
 
@@ -23,14 +24,14 @@ export default function Login() {
   }
 
   const handleSubmit = () => {
-    try{
-      login(username, password).then(r => console.log(r));
+    try {
+      login(username, password).then((r) => console.log(r));
       const success = getClientCookie('tokenDrinking');
-      if(success){
-        console.log('Logged in')
-        router.push('/user/template/home')
+      if (success) {
+        console.log('Logged in');
+        router.push('/user/template/home');
       }
-    }catch(e){
+    } catch (e) {
       setError('En feil oppsto under innloggingen.');
     }
   };
@@ -40,7 +41,10 @@ export default function Login() {
       <div className="w-full max-w-md p-4 border rounded-md bg-white shadow-lg">
         <form className="w-full" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Brukernavn
             </label>
             <Input
@@ -53,7 +57,10 @@ export default function Login() {
           </div>
 
           <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
               Passord
             </label>
             <Input
@@ -69,7 +76,10 @@ export default function Login() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Logg inn
             </button>
-            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
               Glemt passord?
             </a>
           </div>
@@ -78,4 +88,4 @@ export default function Login() {
       </div>
     </div>
   );
-};
+}
