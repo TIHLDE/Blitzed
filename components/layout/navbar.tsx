57c7ef2b2903a46c9f4d57a10b70c432a3d0fb90
henrikhaus <henrik.utistog.hausberg@outlook.com>
2024-01-08 19:03:45 +0100

    Modified navbar, installed shadcn components

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import { ThemeModeToggler } from '@/components/ui/theme-mode-toggler';
 import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { ThemeModeToggler } from '@/components/ui/theme-mode-toggler';
+import { AlignJustify } from 'lucide-react';
 
 export default function Navbar() {
   return (
-    <nav className="bg-primary dark:bg-primaryDark">
+    <nav className="bg-background">
       <div className="flex justify-between items-center p-3 shadow-xl">
-        <button className="w-12 h-12">
-          <span className="material-icons-round text-text dark:text-textDark scale-150 font-bold thickIcon">
-            menu
-          </span>
-        </button>
-        <Logo
-          className="fill-text dark:fill-textDark"
-          width={200}
-          height={'auto'}
-        />
+        <Button size={'icon'} variant={'ghost'}>
+          <AlignJustify size={30} className="fill-primary stroke-[2px]" />
+        </Button>
+        <Logo className="fill-primary" width={200} height={'auto'} />
         <ThemeModeToggler />
       </div>
       <div id="sideBar" className="h-screen w-32 bg-dark hidden"></div>
