caa103835aee76c7af932091d92885a48ba31686
henrikhaus <henrik.utistog.hausberg@outlook.com>
2024-01-08 20:00:03 +0100

    Added menubar, profile avatar and navbar separator

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,19 +1,49 @@
 import Logo from '@/components/logo';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 import { ThemeModeToggler } from '@/components/ui/theme-mode-toggler';
-import { AlignJustify } from 'lucide-react';
+import { AlignJustify, UserRound } from 'lucide-react';
 
 export default function Navbar() {
   return (
-    <nav className="bg-background">
-      <div className="flex justify-between items-center p-3 shadow-xl">
-        <Button size={'icon'} variant={'ghost'}>
-          <AlignJustify size={30} className="fill-primary stroke-[2px]" />
-        </Button>
+    <nav className="bg-background flex flex-col content-start">
+      <div className="flex justify-between items-center p-3 border-b-[1px]">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={'icon'} variant={'ghost'}>
+              <AlignJustify size={30} className="fill-primary stroke-[2px]" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={'left'}>
+            <SheetHeader>
+              <SheetTitle>Side menu</SheetTitle>
+              <SheetDescription>
+                This is a really cool side menu.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
         <Logo className="fill-primary" width={200} height={'auto'} />
-        <ThemeModeToggler />
+        <div className="flex gap-3">
+          <Button size={'icon'} variant={'ghost'}>
+            <Avatar className=" hover:brightness-90 duration-100">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@profile" />
+              <AvatarFallback>
+                <UserRound className="text-foreground" />
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+          <ThemeModeToggler />
+        </div>
       </div>
-      <div id="sideBar" className="h-screen w-32 bg-dark hidden"></div>
     </nav>
   );
 }
