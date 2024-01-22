e92533387c646c20497deb00abd84baf6ec71f24
henrikhaus <henrik.utistog.hausberg@outlook.com>
2024-01-22 16:53:43 +0100

    Squashed commit of the following:
    commit 8c6d9d9bcbc1e77f40706bb334f0791f531435bc
    Author: Anders <anders.morille@gmail.com>
    Date:   Mon Jan 22 16:37:19 2024 +0100
    
        Pages shell for tournament wireframe

--- app/tournament/[id]/page.tsx
+++ app/tournament/[id]/page.tsx
@@ -1,11 +1,7 @@
-import Before from './before';
-
 interface TournamentPageProps {
   params: {
     id: string;
   };
 }
 
-export default function TournamentPage(props: TournamentPageProps) {
-  return <Before />;
-}
+export default function TournamentPage(props: TournamentPageProps) {}
