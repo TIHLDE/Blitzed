4dc2a21a8d869132dfddec87b9eb83ec1ff63694
Jonatan Andre Vevang <jonatanv@stud.ntnu.no>
2024-02-12 18:27:57 +0100

    disabled eslint when building

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    eslint: {
+      // Ignorer ESLint under byggesteg
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
