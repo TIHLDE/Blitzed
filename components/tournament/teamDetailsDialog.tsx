26ae0574b7b10f112575fd4d0cea23679438e1a1
Anders <anders.morille@gmail.com>
2024-02-19 18:00:48 +0100

    :sparkles: Refactored and added join team details dialog

---
+++ components/tournament/teamDetailsDialog.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import React, { ReactNode } from 'react';
+import { Team } from '@/app/tournament/[id]/before';
+
+interface TeamCard {
+  team: Team;
+  children: ReactNode;
+}
+
+export function TeamDetailsDialog({ team, children }: TeamCard) {
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-left">{team.name}</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col gap-4">
+          <Label>Spillere</Label>
+          {team.players.map((player) => (
+            <li key={player}>{player}</li>
+          ))}
+        </div>
+        <DialogFooter>
+          <Button type="submit">Bli med</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
