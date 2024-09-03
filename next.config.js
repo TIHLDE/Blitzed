cdf0fc1064f80aaa684d0575d006e4abdb802fcb
Anders Noël Lothe Morille <anders.morille@gmail.com>
2024-09-03 13:28:21 +0200

    Remove lint ignoring for build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -8,3 +9,11 @@ await import("./src/env.js");
 const config = {};
 
 export default config;
+=======
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {},
+};
+
+module.exports = nextConfig;
+>>>>>>> Stashed changes
