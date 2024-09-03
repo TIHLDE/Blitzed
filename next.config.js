5fd747633808c2de745e9e2ffce1f943724d30ab
Anders Noël Lothe Morille <anders.morille@gmail.com>
2024-09-03 13:34:38 +0200

    Tailwind config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -9,11 +8,3 @@ await import("./src/env.js");
 const config = {};
 
 export default config;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {},
-};
-
-module.exports = nextConfig;
->>>>>>> Stashed changes
