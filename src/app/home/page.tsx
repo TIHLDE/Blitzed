feaac97b687fc45669fed3aa842dcaf54a53de69
sofiefbr <sofiefbr@stud.ntnu.no>
2024-09-26 15:35:37 +0200

    new design on how to choose games

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent, CardTitle } from "../../components/ui/card";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 interface Game {
   link: string;
@@ -21,32 +20,37 @@ const games: Game[] = [
     title: "Beer pong turnering",
     svgSrc: "/beerpong.png",
     alt: "beer pong tournament poster",
-  },
+  }, 
 ];
 
 export default function UserHomePage() {
   return (
     <div className={"flex h-[90vh] w-full flex-col justify-center"}>
-      <h1 className={"text-center text-3xl font-bold text-primary"}>
+      <h1 className={"color:black text-center text-3xl font-bold"}>
         Velg et spill for å fortsette
       </h1>
-      <div className="mt-4 flex h-fit w-full flex-col items-center justify-center gap-4 md:flex-row">
+      <div className="mt-4 flex h-fit w-full flex-col items-center justify-center gap-6 md:flex-row">
         {games.map((cardData, index) => (
-          <Link href={cardData.link} key={cardData.title}>
+          <Link href={cardData.link} key={index}>
             <Card
-              key={index}
               className={
-                "w-96 overflow-hidden duration-100 hover:cursor-pointer hover:shadow-lg"
+                "w-96 transform overflow-hidden transition-transform duration-300 hover:scale-105 hover:cursor-pointer"
               }
             >
-              <img
-                src={cardData.svgSrc}
-                alt={cardData.title}
-                className={"mb-3 h-40 w-full object-cover"}
-              />
-              <CardContent>
-                <CardTitle>{cardData.title}</CardTitle>
-              </CardContent>
+              <div className="relative w-full">
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <span className="rounded bg-black bg-opacity-50 px-4 py-2 text-xl font-bold text-white">
+                    {cardData.title}
+                  </span>
+                </div>
+                <div>
+                  <img
+                    src={cardData.svgSrc}
+                    alt={cardData.alt}
+                    className={"h-40 w-full"}
+                  />
+                </div>
+              </div>
             </Card>
           </Link>
         ))}
