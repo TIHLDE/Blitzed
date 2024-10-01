98d0f812959cac10bdb1f519b5743b2b806ae6b9
FridaKroken <frida.kroken@hotmail.com>
2024-10-01 14:41:19 +0200

    added trpc and started on form

--- src/app/question-game/create/page.tsx
+++ src/app/question-game/create/page.tsx
@@ -1,13 +1,64 @@
+'use client';
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import { Form } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import { InputSchema } from "~/server/api/question-game/game/controller/create";
+import { api } from "~/trpc/react";
 
 export default function CreateQuestionPage() {
+    const form = useForm<z.infer<typeof InputSchema>>({
+        resolver: zodResolver(InputSchema),
+        defaultValues: {
+            title: ""
+        }
+    });
+
+    const {
+        mutate: createGame,
+        status,
+        data: game
+    } = api.questionGame.game.create.useMutation();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "success") {
+            router.replace(`/question-game/${game.questionGameId}/questions`);
+        }
+    }, [status]);
+
+    const onSubmit = (values: z.infer<typeof InputSchema>) => 
+        createGame(values);
+
     return (
-        <div>
-            <Card className="w-[350px]">
+        <main className="w-full h-[80vh] py-12 px-2 space-y-8">
+            <div className="text-center space-y-2">
+                <h1 className="text-2xl font-bold">
+                    Opprett en ny 100 spørsmål
+                </h1>
+                <p className="text-sm text-secondary-foreground">
+                    Her kan du lage din personlige “100 spørsmål”
+                </p>
+            </div>
+            
+            <Form {...form}>
+                <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                >
+
+                
+                </form>
+            </Form>
+
+            {/* <Card className="w-[350px]">
                 <CardHeader>
                     <CardTitle>Opprett en ny 100 spørsmål</CardTitle>
                 </CardHeader>
@@ -27,7 +78,7 @@ export default function CreateQuestionPage() {
                 <CardFooter className="flex justify-between">
                     <Button>Deploy</Button>
                 </CardFooter>
-            </Card>
-        </div>
+            </Card> */}
+        </main>
     )
 }
---
+++ src/server/api/question-game/game/controller/create.ts
@@ -0,0 +1,45 @@
+import { as } from "vitest/dist/chunks/reporters.WnPwkmgA.js";
+import { z } from "zod";
+import { Controller, protectedProcedure } from "~/server/api/trpc";
+import { assertHasCreateQuestionGameControl } from "../../middleware";
+import { db } from "~/server/db";
+
+export const InputSchema = z.object({
+    title: z.string()
+});
+
+export const OutputSchema = z.object({
+    questionGameId: z.string()
+});
+
+const handler: Controller<
+    z.infer<typeof InputSchema>,
+    z.infer<typeof OutputSchema>
+> = async ({input, ctx}) => {
+    await assertHasCreateQuestionGameControl({ctx});
+
+    const game = await db.questionGame.create({
+        data: {
+            title: input.title,
+            user: {
+                connect: {
+                    id: ctx.session.user.id
+                }
+            }
+        },
+        select: {
+            id: true
+        }
+    });
+    
+    return {
+        questionGameId: game.id
+    }
+}
+
+export default protectedProcedure
+    .input(InputSchema)
+    .output(OutputSchema)
+    .mutation(handler);
+
+
