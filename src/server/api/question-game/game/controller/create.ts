a6be8843e315f4aa8244ffab3f95aa89f40c42f5
Anders Noël Lothe Morille <anders.morille@gmail.com>
2024-10-21 19:07:17 +0200

    Upload image for create endpoint

--- src/server/api/question-game/game/controller/create.ts
+++ src/server/api/question-game/game/controller/create.ts
@@ -1,33 +1,28 @@
 import { z } from "zod";
-import { adminProcedure, Controller } from '~/server/api/trpc';
-import { assertHasCreateQuestionGameControl } from "../../middleware";
+import { adminProcedure, Controller } from "~/server/api/trpc";
 import { InputSchema, OutputSchema } from "../schema/create";
 import { db } from "~/server/db";
 
-
 const handler: Controller<
-    z.infer<typeof InputSchema>,
-    z.infer<typeof OutputSchema>
-> = async ({input, ctx}) => {
-    await assertHasCreateQuestionGameControl({ctx});
+  z.infer<typeof InputSchema>,
+  z.infer<typeof OutputSchema>
+> = async ({ input, ctx }) => {
+  const game = await db.questionGame.create({
+    data: {
+      title: input.title,
+      imageUrl: input.imageUrl,
+    },
+    select: {
+      id: true,
+    },
+  });
 
-    const game = await db.questionGame.create({
-        data: {
-            title: input.title,
-        },
-        select: {
-            id: true
-        }
-    });
-    
-    return {
-        questionGameId: game.id
-    }
-}
+  return {
+    questionGameId: game.id,
+  };
+};
 
 export default adminProcedure
-    .input(InputSchema)
-    .output(OutputSchema)
-    .mutation(handler);
-
-
+  .input(InputSchema)
+  .output(OutputSchema)
+  .mutation(handler);
--- src/server/api/question-game/game/schema/create.ts
+++ src/server/api/question-game/game/schema/create.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
 export const InputSchema = z.object({
-    title: z.string()
+  title: z.string(),
+  imageUrl: z.string().url(),
 });
 
 export const OutputSchema = z.object({
-    questionGameId: z.number().int().nonnegative()
+  questionGameId: z.number().int().nonnegative(),
 });
