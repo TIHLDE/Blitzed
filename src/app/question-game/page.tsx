2b722577707098cab05c7877c48a57025cce78df
Anders Noël Lothe Morille <anders.morille@gmail.com>
2024-10-21 19:51:31 +0200

    Question games page with endpoint

--- src/app/question-game/page.tsx
+++ src/app/question-game/page.tsx
@@ -1,24 +1,19 @@
 import GameLinkCard from "../../components/ui/game-link-card";
 import { api } from "../../trpc/server";
 
-export default function QuestionGamesPage() {
-  const questionGame = [];
+export default async function QuestionGamesPage() {
+  const games = await api.questionGame.game.getAll();
 
   return (
     <div className="mt-6 min-h-[100svh]">
       <div className="grid grid-cols-2 gap-6">
-        <GameLinkCard
-          title={"Beerpong"}
-          img={"/img/beer_pong_image.jpg"}
-          url={"/"}
-        />
-        <GameLinkCard
-          title={"100 spørsmål"}
-          img={
-            "https://images.unsplash.com/photo-1558383409-ab7ef8db3e01?q=80&w=1786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          url={"/question-game"}
-        />
+        {games.map((g) => (
+          <GameLinkCard
+            title={g.title}
+            img={g.imageUrl}
+            url={`/question-game/${g.id}`}
+          />
+        ))}
       </div>
     </div>
   );
