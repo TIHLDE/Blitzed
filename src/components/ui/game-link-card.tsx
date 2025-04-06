acd4ecf312b033e086bb9f7a9afa5e95e936a92b
sofiefbr <sofiefbr@stud.ntnu.no>
2025-04-06 21:56:50 +0200

    Changed darkmode colors on gameframes

--- src/components/ui/game-link-card.tsx
+++ src/components/ui/game-link-card.tsx
@@ -14,7 +14,7 @@ export default function GameLinkCard({ title, img, url }: GameLinkCardProps) {
   return (
     <div
       className={
-        "relative aspect-square w-40 cursor-pointer select-none rounded-lg px-1 transition-all duration-100 [box-shadow:0_5px_0_0_#fff,0_15px_0_0_#aaa] hover:translate-y-[-5px] hover:[box-shadow:0_5px_0_0_#fff,0_20px_0_0_#aaa] active:translate-y-3 active:[box-shadow:0_0px_0_0_#444,0_0px_0_0_#aaa]"
+        "relative aspect-square w-40 cursor-pointer select-none rounded-lg px-1 pt-1 dark:bg-[#24496b] transition-all duration-100 [box-shadow:0_5px_0_0_#fff,0_15px_0_0_#aaa] dark:[box-shadow:0_5px_0_0_#24496b,0_15px_0_0_#071f35] hover:translate-y-[-5px] hover:[box-shadow:0_5px_0_0_#fff,0_20px_0_0_#aaa] dark:hover:[box-shadow:0_5px_0_0_#24496b,0_20px_0_0_#071f35] active:translate-y-3 active:[box-shadow:0_0px_0_0_#444,0_0px_0_0_#aaa] dark:active:[box-shadow:0_0px_0_0_#444,0_0px_0_0_#aaa] dark:active:pb-1"
       }
       onClick={() => router.push(url)}
     >
